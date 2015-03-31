aed60ba3c7a94d904d813016a20096b41d9f82a7
Jordan Worona <jworona@Jordans-Mac-mini.local>
2015-03-31 19:32:32 -0300

    Initial Commit

---
+++ alice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  alice
+//
+//  Created by Jordan Worona on 2015-03-31.
+//  Copyright (c) 2015 apollo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
